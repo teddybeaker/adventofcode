006b870f5cf7ab5b905813d2be44e42328f617b0
Stefani Gerber <stefani.gerber@bluesky-it.ch>
2018-03-15 20:50:51 +0100

    day4

---
+++ 2017/day4/check_passphrase.py
@@ -0,0 +1,45 @@
+def is_valid_passphrase(passphrase):
+    words = passphrase.split(" ")
+    for word in words:
+        if words.count(word) > 1:
+            return False
+    return True
+
+def is_valid_passphrase_strict(passphrase):
+    words = passphrase.split(" ")
+    for i in range(0, len(words)):
+        for j in range(i, len(words)):
+            if (i != j):
+                if is_anagram(words[i], words[j]):
+                    return False
+    return True
+
+def is_anagram(word1, word2):
+    if word1 == word2:
+        return True
+    if len(word1) != len(word2):
+        return False
+    for char in word1:
+        if word1.count(char) != word2.count(char):
+            return False;
+    return True
+
+
+
+if __name__ == "__main__":
+    sum1 = sum2 = 0
+    count = 0
+    with open("./input.txt") as f:
+        content = f.read()
+        for line in content.split("\n"):
+            count += 1
+            passphrase = line.strip()
+            if len(passphrase):
+                if is_valid_passphrase(passphrase):
+                    sum1 += 1
+                if is_valid_passphrase_strict(passphrase):
+                    sum2 += 1
+    f.closed
+
+    print("found %d valid passphrases with unique words in %d" % (sum1, count))
+    print("found %d valid passphrases without anagrams in %d" % (sum2, count))
---
+++ 2017/day4/test.py
@@ -0,0 +1,49 @@
+import unittest;
+from check_passphrase import is_valid_passphrase, is_valid_passphrase_strict, is_anagram;
+
+class TestPassPhraseChecker(unittest.TestCase):
+
+    def test_1_1(self):
+        result = is_valid_passphrase("aa bb cc dd ee")
+        self.assertTrue(result)
+
+    def test_1_2(self):
+        result = is_valid_passphrase("aa bb cc dd aa")
+        self.assertFalse(result)
+
+    def test_1_3(self):
+        result = is_valid_passphrase("aa bb cc dd aaa")
+        self.assertTrue(result)
+
+    def test_2_1(self):
+        result = is_valid_passphrase_strict("abcde fghij")
+        self.assertTrue(result)
+
+    def test_2_2(self):
+        result = is_valid_passphrase_strict("abcde xyz ecdab")
+        self.assertFalse(result)
+
+    def test_2_3(self):
+        result = is_valid_passphrase_strict("a ab abc abd abf abj")
+        self.assertTrue(result)
+
+    def test_2_4(self):
+        result = is_valid_passphrase_strict("iiii oiii ooii oooi oooo")
+        self.assertTrue(result)
+
+    def test_2_5(self):
+        result = is_valid_passphrase_strict("oiii ioii iioi iiio")
+        self.assertFalse(result)
+
+    def test_3_1(self):
+        self.assertTrue(is_anagram("abcde", "abcde"))
+
+    def test_3_2(self):
+        self.assertTrue(is_anagram("abcde", "ecdab"))
+
+    def test_3_3(self):
+        self.assertFalse(is_anagram("abcde", "fghij"))
+
+
+if __name__ == "__main__":
+    unittest.main()
