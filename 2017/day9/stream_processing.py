415a4a23dde5eae0aefca4a4456af823deb1a9f1
Stefani Gerber <2244342+teddybeaker@users.noreply.github.com>
2018-05-09 18:47:11 +0200

    add day9 part 2

--- 2017/day9/stream_processing.py
+++ 2017/day9/stream_processing.py
@@ -10,12 +10,19 @@ class StreamProcessing:
         self._current_position = -1
         self._in_garbage = False
         self._group_count = 0
+        self._garbage_chars_count = 0
         self._total_score = 0
         self._next_score = 1
 
     def process(self):
+        self._filter_canceled()
         for self._current_position in range(0, len(self._stream)):
-            if self._group_start():
+            if self._in_garbage:
+                if self._garbage_end():
+                    self._in_garbage = False
+                else:
+                    self._garbage_chars_count += 1
+            elif self._group_start():
                 self._group_count += 1
                 self._total_score += self._next_score
                 self._next_score += 1
@@ -23,38 +30,32 @@ class StreamProcessing:
                 self._next_score -= 1
             elif self._garbage_start():
                 self._in_garbage = True
-            elif self._garbage_end():
-                self._in_garbage = False
+
+    def _filter_canceled(self):
+        clean = []
+        i = 0
+        while i < len(self._stream):
+            if self._stream[i] == '!':
+                i += 2
+            else:
+                clean.append(self._stream[i])
+                i += 1
+        self._stream = ''.join(clean)
 
     def _group_start(self):
-        if self._in_garbage:
-            return False
         return self._is_at(StreamProcessing.GROUP_START)
 
     def _group_end(self):
-        if self._in_garbage:
-            return False
         return self._is_at(StreamProcessing.GROUP_END)
 
     def _garbage_start(self):
-        if self._in_garbage:
-            return False
         return self._is_at(StreamProcessing.GARBAGE_START)
 
     def _garbage_end(self):
-        if not self._in_garbage:
-            return False
         return self._is_at(StreamProcessing.GARBAGE_END)
 
     def _is_at(self, character):
-        if self._stream[self._current_position] != character:
-            return False
-        count_cancels = 0
-        pos = self._current_position-1
-        while self._stream[pos] == '!':
-            count_cancels += 1
-            pos -= 1
-        return count_cancels % 2 == 0
+        return self._stream[self._current_position] == character
 
     def group_count(self):
         return self._group_count
@@ -62,9 +63,13 @@ class StreamProcessing:
     def total_score(self):
         return self._total_score
 
+    def garbage_chars_count(self):
+        return self._garbage_chars_count
+
 
 if __name__ == '__main__':
     with open('./input.txt') as f:
         processor = StreamProcessing(f.read())
         processor.process()
         print("score of input is %s " % processor.total_score())
+        print("garbage contains %s non-canceled characters" % processor.garbage_chars_count())
--- 2017/day9/test.py
+++ 2017/day9/test.py
@@ -9,90 +9,112 @@ class TestStreamProcessing(unittest.TestCase):
         processor.process()
         self.assertEqual(1, processor.group_count())
         self.assertEqual(1, processor.total_score())
+        self.assertEqual(0, processor.garbage_chars_count())
 
     def test_three_groups(self):
         processor = StreamProcessing('{{{}}}')
         processor.process()
         self.assertEqual(3, processor.group_count())
         self.assertEqual(6, processor.total_score())
+        self.assertEqual(0, processor.garbage_chars_count())
 
     def test_three_groups2(self):
         processor = StreamProcessing('{{},{}}')
         processor.process()
         self.assertEqual(3, processor.group_count())
         self.assertEqual(5, processor.total_score())
+        self.assertEqual(0, processor.garbage_chars_count())
 
     def test_six_groups(self):
         processor = StreamProcessing('{{{},{},{{}}}}')
         processor.process()
         self.assertEqual(6, processor.group_count())
         self.assertEqual(16, processor.total_score())
+        self.assertEqual(0, processor.garbage_chars_count())
 
     def test_one_group_with_garbage(self):
         processor = StreamProcessing('{<{},{},{{}}>}')
         processor.process()
         self.assertEqual(1, processor.group_count())
         self.assertEqual(1, processor.total_score())
+        self.assertEqual(10, processor.garbage_chars_count())
 
     def test_one_group_with_garbage2(self):
         processor = StreamProcessing('{<a>,<a>,<a>,<a>}')
         processor.process()
         self.assertEqual(1, processor.group_count())
         self.assertEqual(1, processor.total_score())
+        self.assertEqual(4, processor.garbage_chars_count())
 
     def test_five_groups_with_garbage(self):
         processor = StreamProcessing('{{<a>},{<a>},{<a>},{<a>}}')
         processor.process()
         self.assertEqual(5, processor.group_count())
         self.assertEqual(9, processor.total_score())
+        self.assertEqual(4, processor.garbage_chars_count())
 
     def test_five_groups_with_garbage_cancel(self):
         processor = StreamProcessing('{{<!!>},{<!!>},{<!!>},{<!!>}}')
         processor.process()
         self.assertEqual(5, processor.group_count())
         self.assertEqual(9, processor.total_score())
+        self.assertEqual(0, processor.garbage_chars_count())
 
     def test_two_groups_with_garbage(self):
         processor = StreamProcessing('{{<!>},{<!>},{<!>},{<a>}}')
         processor.process()
         self.assertEqual(2, processor.group_count())
         self.assertEqual(3, processor.total_score())
+        self.assertEqual(13, processor.garbage_chars_count())
 
     def test_two_groups_with_garbage2(self):
         processor = StreamProcessing('{{<a!>},{<a!>},{<a!>},{<ab>}}')
         processor.process()
         self.assertEqual(2, processor.group_count())
         self.assertEqual(3, processor.total_score())
+        self.assertEqual(17, processor.garbage_chars_count())
 
     def test_garbage(self):
         processor = StreamProcessing('<>')
         processor.process()
         self.assertEqual(0, processor.group_count())
+        self.assertEqual(0, processor.total_score())
+        self.assertEqual(0, processor.garbage_chars_count())
 
     def test_garbage2(self):
         processor = StreamProcessing('<<<<>')
         processor.process()
         self.assertEqual(0, processor.group_count())
+        self.assertEqual(0, processor.total_score())
+        self.assertEqual(3, processor.garbage_chars_count())
 
     def test_garbage_canceled_group(self):
         processor = StreamProcessing('<{!>}>')
         processor.process()
         self.assertEqual(0, processor.group_count())
+        self.assertEqual(0, processor.total_score())
+        self.assertEqual(2, processor.garbage_chars_count())
 
     def test_garbage_canceled_cancel(self):
         processor = StreamProcessing('<!!>')
         processor.process()
         self.assertEqual(0, processor.group_count())
+        self.assertEqual(0, processor.total_score())
+        self.assertEqual(0, processor.garbage_chars_count())
 
     def test_garbage_canceled_cancel2(self):
         processor = StreamProcessing('<!!!>>')
         processor.process()
         self.assertEqual(0, processor.group_count())
+        self.assertEqual(0, processor.total_score())
+        self.assertEqual(0, processor.garbage_chars_count())
 
     def test_garbage3(self):
         processor = StreamProcessing('<{o"i!a,<{i<a>')
         processor.process()
         self.assertEqual(0, processor.group_count())
+        self.assertEqual(0, processor.total_score())
+        self.assertEqual(10, processor.garbage_chars_count())
 
 
 if __name__ == '__main__':
