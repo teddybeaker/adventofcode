909bf20d6ee0098e5109827ea1222edbf7e975a9
Stefani Gerber <2244342+teddybeaker@users.noreply.github.com>
2018-08-16 06:13:37 +0200

    day 13 part 2

--- 2017/day13/firewall.py
+++ 2017/day13/firewall.py
@@ -1,20 +1,12 @@
 class Firewall:
 
     def __init__(self, layers):
-
         self._layers = layers
+        self._max_key = list(self._layers.keys())[-1]
 
     def calculate_severity(self):
-
-        max_key = list(self._layers.keys())[-1]
-
         severity = 0
-
-        # ignore level 0 since it doesn't contribute to severity (multiplication by 0)
-        if 0 in self._layers:
-            print('hit on layer 0')
-
-        for depth in range(1, max_key+1):
+        for depth in range(0, self._max_key+1):
             if depth in self._layers:
                 position_in_range = depth % (2*self._layers[depth] - 2)
                 if position_in_range == 0:
@@ -24,14 +16,40 @@ class Firewall:
 
         return severity
 
+    def get_safe_path(self):
+        offset = 0
+        while True:
+            if self._is_hit(offset):
+                offset += 1
+            else:
+                return offset
+            if offset > 20000000:  # manually incremented to prevent infinte loop
+                return -1
+
+    def _is_hit(self, offset):
+        for depth in range(0, self._max_key+1):
+            if depth in self._layers:
+                position_in_range = (depth + offset) % (2*self._layers[depth] - 2)
+                if position_in_range == 0:
+                    # print('hit on layer %s with offset %s' % (depth, offset))
+                    return True
+        return False
+
+
+def parse_layers(file_content):
+    layers = {}
+    for line in file_content.split('\n'):
+        # don't call this range! will owerwrite function range
+        dep, rng = line.split(': ')
+        layers[int(dep)] = int(rng)
+    return layers
+
+
 if __name__ == '__main__':
     with open('./input.txt') as f:
         content = f.read().strip()
-        layers = {}
-        for line in content.split('\n'):
-            # don't call this range! will owerwrite function range
-            depth, rng = line.split(': ')
-            layers[int(depth)] = int(rng)
-        firewall = Firewall(layers)
-        severity = firewall.calculate_severity()
-        print('severity is %s' % severity)
+        firewall = Firewall(parse_layers(content))
+        sev = firewall.calculate_severity()
+        print('severity is %s' % sev)
+        delay = firewall.get_safe_path()
+        print('offset for safe passage is %s' % delay)
--- 2017/day13/test.py
+++ 2017/day13/test.py
@@ -2,16 +2,26 @@ import unittest
 
 from firewall import Firewall
 
+
 class FirewallTest(unittest.TestCase):
 
-    def test(self):
+    def test_smoke_severity(self):
         layers = {0: 3,
                   1: 2,
                   4: 4,
                   6: 4}
         firewall = Firewall(layers)
         severity = firewall.calculate_severity()
-        self.assertEqual(severity, 24) # 0*3 + 6*4
+        self.assertEqual(severity, 24)  # 0*3 + 6*4
+
+    def test_smoke_safe_path(self):
+        layers = {0: 3,
+                  1: 2,
+                  4: 4,
+                  6: 4}
+        firewall = Firewall(layers)
+        offset = firewall.get_safe_path()
+        self.assertEqual(offset, 10)
 
 
 if __name__ == '__main__':
